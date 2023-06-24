484877a9d14b818d129c497815b8a7a0ed6cdcc6
H.V.Linh <hvl052k2@gmail.com>
2023-06-24 15:16:52 +0700

    draw block by line, add gui controls

--- main.js
+++ main.js
@@ -1,22 +1,21 @@
-import Stats from "./libs/stats.module.js";
-
 // Tạo scene, camera và renderer
 const scene = new THREE.Scene();
+const gui = new dat.GUI();
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
+
 const renderer = new THREE.WebGLRenderer();
-const gui = new dat.GUI();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const blockMap = {
   box: {
     geometry: (params) =>
-      new THREE.BoxGeometry(params.width, params.height, params.depth),
+      new THREE.BoxGeometry(params.width, params.height, params.depth, params.widthSegments, params.heightSegments),
   },
   sphere: {
     geometry: (params) =>
@@ -53,7 +52,7 @@ const blockMap = {
       ),
   },
   plane: {
-    geometry: (params) => new THREE.PlaneGeometry(params.width, params.height),
+    geometry: (params) => new THREE.PlaneGeometry(params.width, params.height, params.widthSegments, params.heightSegments),
   },
 };
 
@@ -81,102 +80,42 @@ const materialMap = {
 
 function drawBlock(config) {
   const geometry = blockMap[config.nameBlock].geometry(config.params);
-  const material = materialMap[config.nameMaterial](
-    config.color,
-    config.texture
-  );
-  let block;
-  if (config.nameBlock === "sphere") {
-    if (config.nameMaterial === "line") {
-      const vertices = geometry.attributes.position.array;
-      const indices = geometry.index.array;
-
-      const verticesCount = vertices.length / 3;
-      const lines = [];
-
-      for (let i = 0; i < indices.length; i += 3) {
-        const v1 = indices[i] * 3;
-        const v2 = indices[i + 1] * 3;
-        const v3 = indices[i + 2] * 3;
-
-        const line1 = new THREE.Line3(
-          new THREE.Vector3(vertices[v1], vertices[v1 + 1], vertices[v1 + 2]),
-          new THREE.Vector3(vertices[v2], vertices[v2 + 1], vertices[v2 + 2])
-        );
-
-        const line2 = new THREE.Line3(
-          new THREE.Vector3(vertices[v2], vertices[v2 + 1], vertices[v2 + 2]),
-          new THREE.Vector3(vertices[v3], vertices[v3 + 1], vertices[v3 + 2])
-        );
-
-        const line3 = new THREE.Line3(
-          new THREE.Vector3(vertices[v3], vertices[v3 + 1], vertices[v3 + 2]),
-          new THREE.Vector3(vertices[v1], vertices[v1 + 1], vertices[v1 + 2])
-        );
-
-        lines.push(line1, line2, line3);
-      }
-
-      const geometryLines = new THREE.BufferGeometry().setFromPoints(
-        lines.flatMap((line) => [line.start, line.end])
-      );
-
-      block = new THREE.LineSegments(geometryLines, material);
-    } else {
-      block = new THREE.Mesh(geometry, material);
-    }
+  let block, material;
+  if (config.nameMaterial === "line") {
+    material = new THREE.LineBasicMaterial({ color: config.color });
+    const wireframe = new THREE.WireframeGeometry(geometry);
+    block = new THREE.LineSegments(wireframe, material);
+    block.material.depthTest = false;
+    block.material.opacity = 0.5;
+    block.material.transparent = true;
+  } else {
+    material = materialMap[config.nameMaterial](
+      config.color,
+      config.texture
+    );
+    block = new THREE.Mesh(geometry, material);
   }
-  if (config.nameBlock === "plane") {
-    if (config.nameMaterial === "line") {
-      const halfWidth = config.params.width / 2;
-      const halfHeight = config.params.height / 2;
-      const segmentWidth = config.params.width / 20;
-      const segmentHeight = config.params.height / 20;
 
-      const vertices = [];
-      const indices = [];
-
-      for (let i = 0; i <= 20; i++) {
-        const y = i * segmentHeight - halfHeight;
-
-        for (let j = 0; j <= 20; j++) {
-          const x = j * segmentWidth - halfWidth;
-
-          vertices.push(x, y, 0);
-
-          if (i < 20 && j < 20) {
-            const currentIndex = i * (20 + 1) + j;
-            const nextIndex = currentIndex + 20 + 1;
-
-            indices.push(currentIndex, currentIndex + 1, nextIndex);
-            indices.push(nextIndex, currentIndex + 1, nextIndex + 1);
-
-            // Đường thẳng dọc
-            indices.push(currentIndex, nextIndex, currentIndex + 1);
-            indices.push(currentIndex + 1, nextIndex, nextIndex + 1);
-          }
-        }
-      }
-
-      const geometry = new THREE.BufferGeometry();
-      geometry.setAttribute(
-        "position",
-        new THREE.Float32BufferAttribute(vertices, 3)
-      );
-      geometry.setIndex(indices);
-
-      const material = new THREE.LineBasicMaterial({ color: config.color });
-
-      block = new THREE.LineSegments(geometry, material);
-    }
-  }
   scene.add(block);
-  return { geometry, block };
+  return { geometry, material, block };
 }
+
+const boxConfig = {
+  nameBlock: "box",
+  nameMaterial: "standard",
+  params: {
+    width: 4,
+    height: 4,
+    depth: 4,
+    widthSegments:15,
+    heightSegments:15,
+  },
+  color: 0xffffff,
+};
 
 const sphereConfig = {
   nameBlock: "sphere",
-  nameMaterial: "line",
+  nameMaterial: "standard",
   params: {
     radius: 2,
     widthSegments: 32,
@@ -186,23 +125,12 @@ const sphereConfig = {
   // texture: "./assets/images/ball.jpg",
 };
 
-const boxConfig = {
-  nameBlock: "box",
-  nameMaterial: "line",
-  params: {
-    width: 2,
-    height: 2,
-    depth: 2,
-  },
-  color: 0xffffff,
-};
-
 const coneConfig = {
   nameBlock: "cone",
-  nameMaterial: "basic",
+  nameMaterial: "line",
   params: {
-    radius: 1,
-    height: 2,
+    radius: 2,
+    height: 4,
     radialSegments: 32,
   },
   color: 0xffffff,
@@ -210,23 +138,23 @@ const coneConfig = {
 
 const cylinderConfig = {
   nameBlock: "cylinder",
-  nameMaterial: "basic",
+  nameMaterial: "line",
   params: {
-    radiusTop: 1,
-    radiusBottom: 1,
-    height: 2,
-    radialSegments: 32,
+    radiusTop: 2,
+    radiusBottom: 2,
+    height: 4,
+    radialSegments: 64,
   },
   color: 0xffffff,
 };
 
 const torusConfig = {
   nameBlock: "torus",
-  nameMaterial: "basic",
+  nameMaterial: "line",
   params: {
-    radius: 0.5,
-    tube: 0.25,
-    radialSegments: 16,
+    radius: 3,
+    tube: 2,
+    radialSegments: 32,
     tubularSegments: 32,
   },
   color: 0xffffff,
@@ -238,13 +166,16 @@ const planeConfig = {
   params: {
     width: 20,
     height: 20,
+    widthSegments: 15,
+    heightSegments: 15,
   },
-  color: 0x00ff00,
+  color: 0xffffff,
 };
 
+// Ánh sáng
 const ambientLight = new THREE.AmbientLight(0x333333);
 const pointLight = new THREE.PointLight(0xffffff, 1.5);
-pointLight.position.set(1, 3);
+pointLight.position.set(3,7,1);
 scene.add(pointLight);
 scene.add(ambientLight);
 
@@ -259,22 +190,63 @@ var urls = [
   path + "pz" + format,
   path + "nz" + format,
 ];
+
 var reflectionCube = new THREE.CubeTextureLoader().load(urls);
 reflectionCube.format = THREE.RGBAFormat;
 scene.background = reflectionCube;
 
-// const box = drawBlock(boxConfig);
-// box.block.position.y = 1;
-const sphere = drawBlock(sphereConfig);
-sphere.block.position.y = 1;
+const box = drawBlock(boxConfig);
+box.block.position.y = 1;
+
+// const sphere = drawBlock(sphereConfig);
+// sphere.block.position.y = 1;
+
+// const torus = drawBlock(torusConfig);
+// torus.block.position.y = 1;
+
+// const cylinder = drawBlock(cylinderConfig);
+
+// const cone = drawBlock(coneConfig);
+
 const plane = drawBlock(planeConfig);
+plane.block.position.y = -2;
 plane.block.rotation.x = -Math.PI / 2;
 
-// Đặt camera
+// Hàm đổi màu chất liệu
+// const data = {
+//   color: sphere.material.color.getHex(),
+//   mapsEnabled: true
+// }
+// const colorFolder = gui.addFolder("Color");
+// colorFolder.addColor(data, "color").onChange(() => {
+//   sphere.material.color.setHex(
+//     Number(data.color.toString().replace("#", "0x"))
+//   );
+// });
+
+
 const controls = new THREE.OrbitControls(camera, renderer.domElement);
 controls.update();
-camera.position.set(2, 5, 6);
+
+// Đặt camera
+camera.position.set(3, 5, 10);
 camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+const cameraFolder = gui.addFolder("Camera");
+cameraFolder.add(camera.position, "x", -5, 10);
+cameraFolder.add(camera.position, "y", -5, 20);
+cameraFolder.add(camera.position, "z", -5, 20);
+
+const lightdata = {
+  color: pointLight.color.getHex(),
+  mapsEnabled: true
+}
+
+const lightFolder = gui.addFolder("Light");
+lightFolder.addColor(lightdata, 'color').onChange(() => {
+  pointLight.color.setHex(Number(lightdata.color.toString().replace("#", "0x")));
+})
+lightFolder.add(pointLight,"intensity",0,4);
 
 // Hàm render
 function render(renderer, scene, camera) {
